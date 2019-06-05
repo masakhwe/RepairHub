48d1e71bb0d0163832f2635c9649f5a74d82cef0
Emmanuel Masakhwe <markmanu21@gmail.com>
2019-06-05 17:29:54 +0300

    [Chore #166097423] Added postres database

--- repairhub/settings.py
+++ repairhub/settings.py
@@ -77,11 +77,22 @@ WSGI_APPLICATION = 'repairhub.wsgi.application'
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'repairhub',
+        'USER': 'MANU',
+        'PASSWORD': 'manu2741',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
+
+'''DATABASES = {
+    'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
+'''
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
